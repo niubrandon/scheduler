c2e0e0e2dc91004cf3866536313b7490c6f9e78d
niubrandon <niubrandon@gmail.com>
2021-11-01 23:34:51 -0400

    refactored and move application logic to useApplicationDate

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,56 +5,16 @@ import DayList from "./DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from 'helpers/selectors.js'
 import { useApplicationData } from 'hooks/useApplicationDate';
-
 import axios from "axios";
 
 
 
 export default function Application(props) {
 
-
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  })
+  const {state, setState, setDay, bookInterview, deleteInterview} = useApplicationData();
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const dailyInterviewers = getInterviewersForDay(state, state.day);
-
-  const setDay = day => setState(prev => ({ ...prev, day}));
-
-  useEffect(() => {
-    Promise.all([
-      Promise.resolve(axios.get('/api/days')),
-      Promise.resolve(axios.get('/api/appointments')),
-      Promise.resolve(axios.get('/api/interviewers')),
-    ]).then((all) => {
-      const [first, second, third] = all;  
-      setState(prev => ({ ...prev, days: first.data, appointments: second.data, interviewers: third.data }))
-    });
-
-
-  }, []);
-
-  const bookInterview = (id, interview) => {
-    console.log("bookInterview invoked");
-    console.log("reading the input params from bookinterview", id, interview);
-
-    return axios.put(`/api/appointments/${id}`, { interview: interview });
-    
-  }
-
-
-  const deleteInterview = (id) => {
-    console.log("deleteInterview triggered for appointment", id);
-    return axios.delete(`/api/appointments/${id}`);
-  };
-
-  useEffect(() => {
-    console.log("state changed to", state);
-  },[state])
 
   const renderAppointments = dailyAppointments.map(appointment => {
 
--- src/hooks/useApplicationDate.js
+++ src/hooks/useApplicationDate.js
@@ -1,6 +1,46 @@
 import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
-export default function useApplicationData() {
+export function useApplicationData() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  })
+
+  const setDay = day => setState(prev => ({ ...prev, day}));
+
+  useEffect(() => {
+    Promise.all([
+      Promise.resolve(axios.get('/api/days')),
+      Promise.resolve(axios.get('/api/appointments')),
+      Promise.resolve(axios.get('/api/interviewers')),
+    ]).then((all) => {
+      const [first, second, third] = all;  
+      setState(prev => ({ ...prev, days: first.data, appointments: second.data, interviewers: third.data }))
+    });
 
 
+  }, []);
+
+  const bookInterview = (id, interview) => {
+    console.log("bookInterview invoked");
+    console.log("reading the input params from bookinterview", id, interview);
+
+    return axios.put(`/api/appointments/${id}`, { interview: interview });
+    
+  }
+
+
+  const deleteInterview = (id) => {
+    console.log("deleteInterview triggered for appointment", id);
+    return axios.delete(`/api/appointments/${id}`);
+  };
+
+  useEffect(() => {
+    console.log("state changed to", state);
+  },[state])
+
+  return {state, setState, setDay, bookInterview, deleteInterview}
 };
